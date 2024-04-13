1839d1206b186634bdbc1df558eec1a32e6c2292
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-04-13 17:58:17 -0400

    Finished routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,13 +6,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll()
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(400).json(err))
 });
 
-// get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // find one product by its `id` value
+  Product.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(400).json(err))
 });
 
 // create new product
@@ -94,6 +101,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(400).json(err))
 });
 
 module.exports = router;
